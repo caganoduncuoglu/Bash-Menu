032076c09cc49ea1318be7b93a9a74e926c93bb4
neur1 <49001811+neur1@users.noreply.github.com>
2020-11-18 22:27:21 +0300

    Organized Files (4) Section Completed

--- myprogram4.sh
+++ myprogram4.sh
@@ -1,1 +1,47 @@
 #!/bin/bash
+#
+# CSE3033 Project 1 - Problem 4
+#
+
+filename="$1" #takes filename parameter
+while read -n1 c; #read file character by character
+do
+ 
+  case $c in #and in the switch case sed command changes the matched number to letters and -i updates the file
+    0)
+        sed -i 's/0/zero/g' $1 #finds character after /s (0) and changes into zero and /g is for global change 
+		;;
+	1)
+		sed -i 's/1/one/g' $1
+		;;
+    2)
+		sed -i 's/2/two/g' $1
+		;;
+	3)
+		sed -i 's/3/three/g' $1
+		;;
+    4)
+		sed -i 's/4/four/g' $1
+		;;
+	5)
+		sed -i 's/5/five/g' $1
+		;;
+    6)
+		sed -i 's/6/six/g' $1
+		;;
+	7)
+		sed -i 's/7/seven/g' $1
+		;;
+    8)
+		sed -i 's/8/eight/g' $1
+		;;
+	9)
+		sed -i 's/9/nine/g' $1  
+		;;
+
+	*) #in other cases, do nothing		
+		;;
+        
+  esac
+ 
+done < "$filename"
